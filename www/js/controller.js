b77e50273f535300e40d97e365325abb526825e8
thapapratyush <thapa.pratyush10@gmail.com>
2016-10-12 13:29:30 -0400

    Refactor and add the functionality to log the names of the desired places using the places API.

--- www/js/controller.js
+++ www/js/controller.js
@@ -10,8 +10,8 @@ if ( $scope.date.getHours() < 12 )  { Greetings="Good Morning!"; }
         else { Greetings = "I'm not sure what time it is!"; }
 $scope.saveDetails = function(){
     var name = $scope.user.name;
-                    var firebaseObj = firebase.database().ref('Name');
-        var fb = firebaseObj.push();
+    var firebaseObj = firebase.database().ref('Name');
+    var fb = firebaseObj.push();
 	fb.push({
     Name: name
 	}).then(function(ref) {
@@ -37,19 +37,15 @@ $scope.saveDetails = function(){
     return {
         restrict: 'A',
         link:function(scope, element, attrs){
-                    
- 
           var zValue = scope.$eval(attrs.zoom);
           var lat = scope.$eval(attrs.lat);
           var lng = scope.$eval(attrs.lng);
-
-        
         var myLatlng = new google.maps.LatLng(lat,lng);
         mapOptions = {
                   zoom: zValue,
                   center: myLatlng
                 }
-var image = 'http://melbourneairport.com.au/images/airport-parking/hardhat/parking-options/icon-car.jpg';
+    var image = 'http://melbourneairport.com.au/images/airport-parking/hardhat/parking-options/icon-car.jpg';
     map = new google.maps.Map(element[0],mapOptions);
     marker = new google.maps.Marker({
         icon: image,
@@ -75,32 +71,27 @@ function callback(results, status) {
   }
 };
       function createMarker(place) {
+        /*var List = [];
+        for(i = 0; i < 10; i++)
+            List.push(place.name);
+            console.log(List);
+        }*/
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
           map: map,
           position: place.geometry.location
         });
-
         google.maps.event.addListener(marker, 'click', function() {
           infowindow.setContent(place.name);
           infowindow.open(map, this);
         });
-      }
 
-        scope.printList = function(place){
-            console.log(place.name);
-        var List = [];
-        while(place.name!=""){
-            List.append(place.name);
-            console.log(List);
-        }
-          $scope.showAlert = function() {
+        /*$scope.printList = function(List){
         $ionicPopup.alert({
-        title: "List of restaurants Nearby"  ,
+        title: "List of restaurants Nearby" ,
         template: List
-    })
-      }
-  }
+    })*/
+      };
    
         scope.carParked = function(){
         var firebaseObj = firebase.database().ref('location');
